fdf82606748def7356e92b4761782676e8240b1a
Liangqin Sun <liansun@microsoft.com>
2019-02-12 12:09:17 -0800

    Alt+` to show the quick notes

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,22 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Extension "quick-notes" is activating.');
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders === undefined || workspaceFolders.length <= 0) {
+        return
+    }
 
-    let disposable = vscode.commands.registerCommand('extension.toggleQuickNotes', () => {
-        vscode.window.showInformationMessage('Hello World!');
+    const notesFilePath = path.join(workspaceFolders[0].uri.fsPath, '.vscode', 'notes.txt');
+
+    let disposable = vscode.commands.registerCommand('quicknotes.show', () => {
+        const workspaceEdit = new vscode.WorkspaceEdit();
+        workspaceEdit.createFile(vscode.Uri.file(notesFilePath), { overwrite: false, ignoreIfExists: true });
+        vscode.workspace.applyEdit(workspaceEdit).then(success => {
+            vscode.workspace.openTextDocument(notesFilePath).then(document => {
+                vscode.window.showTextDocument(document);
+            });
+        });
     });
 
     context.subscriptions.push(disposable);
