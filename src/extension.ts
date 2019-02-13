141332d5874f3271d48cc81c3921d18623943cbe
Liangqin Sun <liansun@microsoft.com>
2019-02-12 21:30:51 -0800

    Add file name setting

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,17 @@ import * as path from 'path';
 export function activate(context: vscode.ExtensionContext) {
     const workspaceFolders = vscode.workspace.workspaceFolders;
     if (workspaceFolders === undefined || workspaceFolders.length <= 0) {
-        return
+        return;
     }
 
-    const notesFilePath = path.join(workspaceFolders[0].uri.fsPath, '.vscode', 'notes.txt');
-
     let disposable = vscode.commands.registerCommand('quicknotes.show', () => {
+        const notesFileName = vscode.workspace.getConfiguration().get<string>('quicknotes.fileName');
+        if (notesFileName === undefined) {
+            return;
+        }
+
         const workspaceEdit = new vscode.WorkspaceEdit();
+        const notesFilePath = path.join(workspaceFolders[0].uri.fsPath, '.vscode', notesFileName);
         workspaceEdit.createFile(vscode.Uri.file(notesFilePath), { overwrite: false, ignoreIfExists: true });
         vscode.workspace.applyEdit(workspaceEdit).then(success => {
             vscode.workspace.openTextDocument(notesFilePath).then(document => {
