6b6fcde70b663aa3752751d677c96395e8baf8b1
Liangqin Sun <liansun@microsoft.com>
2019-07-14 22:40:42 -0700

    Update check

--- src/extension.ts
+++ src/extension.ts
@@ -4,18 +4,18 @@ import * as path from 'path';
 export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('quicknotes.toggle', () => {
         const workspaceFolders = vscode.workspace.workspaceFolders;
-        if (workspaceFolders === undefined || workspaceFolders.length === 0) {
+        if (!workspaceFolders || workspaceFolders.length === 0) {
             return;
         }
 
         const notesFileName = vscode.workspace.getConfiguration().get<string>('quicknotes.fileName');
-        if (notesFileName === undefined) {
+        if (!notesFileName) {
             return;
         }
 
         const activeTextEditor = vscode.window.activeTextEditor;
         const notesFilePath = path.join(workspaceFolders[0].uri.fsPath, notesFileName);
-        if (activeTextEditor === undefined || activeTextEditor.document.fileName !== notesFilePath) {
+        if (!activeTextEditor || activeTextEditor.document.fileName !== notesFilePath) {
             const workspaceEdit = new vscode.WorkspaceEdit();
             workspaceEdit.createFile(vscode.Uri.file(notesFilePath), { overwrite: false, ignoreIfExists: true });
             vscode.workspace.applyEdit(workspaceEdit).then(() => {
